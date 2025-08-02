63db69d6667a8973b6b9096f17904ee40b35f8c4
AnmAsifurRahman <asif.120158@gmail.com>
2025-08-02 19:41:52 +0600

    mobile content top fixed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,7 +6,7 @@ import FooterSection from "./_components/FooterSection";
 const HomePage = () => {
   return (
     <div className=" relative flex flex-col items-center lg:items-start">
-      <div className=" flex flex-col mt-80 lg:mt-[57px] ml-0 lg:ml-[215px] max-w-97.5 lg:max-w-full w-full lg:w-139 mb-10  ">
+      <div className=" flex flex-col mt-76 lg:mt-[57px] ml-0 lg:ml-[215px] max-w-97.5 lg:max-w-full w-full lg:w-139 mb-10  ">
         <div className=" flex flex-col  gap-7.5  px-5 lg:px-0 ">
           <HeadingSection />
           <StartSection />
