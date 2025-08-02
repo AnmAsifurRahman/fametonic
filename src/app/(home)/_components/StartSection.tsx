52110956fe2c839cb785a7294ac4ed24c036b2e8
AnmAsifurRahman <asif.120158@gmail.com>
2025-08-02 18:47:48 +0600

    cursor pointer added in button

--- src/app/(home)/_components/StartSection.tsx
+++ src/app/(home)/_components/StartSection.tsx
@@ -5,7 +5,7 @@ import chevronIcon from "@/../public/image/chevron.svg";
 const StartSection = () => {
   return (
     <div className=" flex flex-col lg:w-[313px] gap-2.5 order-3 lg:order-2">
-      <button className=" flex flex-row items-center justify-center w-full max-w-[350px] lg:w-[313px] min-h-10 rounded-[10px] py-2 px-10 gap-2.5 bg-[#FC004E] [box-shadow:_2px_2px_10px_0px_rgba(0_231_249_/_1)]">
+      <button className=" flex flex-row items-center justify-center w-full max-w-[350px] lg:w-[313px] min-h-10 rounded-[10px] py-2 px-10 gap-2.5 bg-[#FC004E] [box-shadow:_2px_2px_10px_0px_rgba(0_231_249_/_1)] cursor-pointer">
         <span className=" font-urbanist lg:font-figtree font-bold text-xl leading-[100%] tracking-normal align-middle">
           GET STARTED
         </span>
